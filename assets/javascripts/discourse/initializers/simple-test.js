782ab9937f48ede03b9a18e39702df3aca6ea992
hectorsanchez <hectorsanchez.mov@gmail.com>
2025-07-09 16:21:49 -0300

    Fix BBB plugin for Discourse 3.5 - Update to component-based modal API

--- assets/javascripts/discourse/initializers/simple-test.js
+++
@@ -1,8 +0,0 @@
-console.log("SIMPLE TEST: Loading...");
-
-export default {
-  name: "simple-test",
-  initialize() {
-    console.log("SIMPLE TEST: Initialized!");
-  }
-};
--- assets/javascripts/initializers/discourse-bbb-test.js
+++
@@ -1,6 +0,0 @@
-export default {
-  name: "discourse-bbb-test",
-  initialize() {
-    console.log("BBB PLUGIN TEST: Working from main initializers!");
-  }
-};
